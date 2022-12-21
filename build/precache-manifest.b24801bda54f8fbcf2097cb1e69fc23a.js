acc26eca39d97d7b08a9898385b6dac32171fb94
Abhishek kumar <abhishekzadav@Gmail.com>
2022-12-21 13:01:16 +0530

    shopping cart

---
+++ build/precache-manifest.b24801bda54f8fbcf2097cb1e69fc23a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27347ef9030cf36c00de8c6bb993e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c1f7d903bb78013daa",
+    "url": "/static/css/main.643891d5.chunk.css"
+  },
+  {
+    "revision": "be9885141e5150a61391",
+    "url": "/static/js/2.440bbd99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.440bbd99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5c1f7d903bb78013daa",
+    "url": "/static/js/main.ca020127.chunk.js"
+  },
+  {
+    "revision": "ba8e8a51f7732c5dade4",
+    "url": "/static/js/runtime-main.44bfc771.js"
+  }
+]);
